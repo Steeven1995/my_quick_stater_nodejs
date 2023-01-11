b2b3d38865999b5893e87411539e223a9ae1fd56
Steeven1995 <gabinmoundziegou@gmail.com>
2023-01-11 23:11:31 +0100

    add a crud company

--- routes/api.company.js
+++ routes/api.company.js
@@ -5,5 +5,8 @@ const isAuthenticateWithToken = require('../utils/isAuthenticateWithToken')
 
 
 router.post('/', isAuthenticateWithToken, CompanyController.create)
+router.get('/my', isAuthenticateWithToken, CompanyController.findOne)
+router.post('/update', isAuthenticateWithToken, CompanyController.update)
+router.get('/delete', isAuthenticateWithToken, CompanyController.delete)
 
 module.exports = router;
